68f5189e8b71519d8d53a772b3fcdfd0d21e73d5
Bird <mostafa_shaeri_tj@yahoo.com>
2024-04-07 21:46:48 +0300

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+
+setup_args = dict(
+    name='azure-iothub',
+    version='0.1.0',
+    description='Azure iot hub api with azure cli backend',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='GNU',
+    packages=find_packages(),
+    author='M.Shaeri',
+    
+    keywords=['Azure', 'IoT', 'Azure IoT hub', 'azure cli'],
+    url='https://github.com/birddevelper/azure-iot-hub-api',
+    download_url='https://github.com/birddevelper/azure-iot-hub-api'
+)
+
+install_requires = [
+    'azure-cli[azure-iot]'
+]
+
+if __name__ == '__main__':
+	setup(**setup_args, install_requires=install_requires)
